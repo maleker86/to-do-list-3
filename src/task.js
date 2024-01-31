8132f6b65db46bb7cf118755b5b3958aa55dd017
Kris <109919530+maleker86@users.noreply.github.com>
2024-01-30 20:33:22 -0800

    Instantiate all required forms. Submit only clears data. Does not submit.

--- src/task.js
+++ src/task.js
@@ -8,28 +8,40 @@ export function createForm() {
     document.body.append(form);
     form.prepend(h1);
     h1.innerText = "New Task:"
-    form.appendChild(field_set);
-    field_set.appendChild(field_legend);
-    field_legend.innerText = "Task:";
+    // form.appendChild(field_set);
+    // field_set.appendChild(field_legend);
+    // field_legend.innerText = "Task:";
 
 
     // create anything
-    function createFormElement(item_id, name, label_text,element_tag, element_type) {
+    function createFormElement(item_id, name, label_text, element_tag, element_type) {
       const field_label = document.createElement("label");
       const element_itself = document.createElement(element_tag);
 
-      field_set.appendChild(field_label);
-      field_set.appendChild(element_itself);
+      form.appendChild(field_label);
+      form.appendChild(element_itself);
 
+      element_itself.setAttribute("id", item_id);
+      element_itself.setAttribute("name", name);
+      element_itself.setAttribute("label", label_text);
       field_label.innerText = '\n' + label_text;
       element_itself.setAttribute("type", element_type);
-      element_itself.setAttribute("label", label_text);
-      element_itself.setAttribute("id", item_id);
-      element_itself.setAttribute("name", name)
+
     }
 
     createFormElement("title","title_input","Title:","input","text");
     createFormElement("desc","description_input","Description:","textarea");
+    createFormElement("due","due_date_input","Due Date:","input","date");
+    createFormElement("priority","priority_number_input","Priority:","input","number");
+
+    //stragglers
+
+    const priority = document.getElementById("priority");
+    const priority_min = 1
+    const priority_max = 3;
+    priority.setAttribute("min",priority_min);
+    priority.setAttribute("max",priority_max);
+
   }
 
   // let form_element_count = 2;
