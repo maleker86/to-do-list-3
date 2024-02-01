7baaa680280de7880b3b872e47c53c7e3ca46141
Kris <109919530+maleker86@users.noreply.github.com>
2024-01-31 21:31:21 -0800

    Clear Title & Desc. Constructor doesn't push to Array BUG

--- src/task.js
+++ src/task.js
@@ -7,15 +7,20 @@ export function createForm() {
 
     document.body.prepend(form);
     form.prepend(h1);
-    h1.innerText = "New Task:"
-    form.setAttribute("id","form");
+    h1.innerText = "New Task:";
+    form.setAttribute("id", "form");
     // form.appendChild(field_set);
     // field_set.appendChild(field_legend);
     // field_legend.innerText = "Task:";
 
-
     // create anything
-    function createFormElement(item_id, name, label_text, element_tag, element_type) {
+    function createFormElement(
+      item_id,
+      name,
+      label_text,
+      element_tag,
+      element_type,
+    ) {
       const field_label = document.createElement("label");
       const element_itself = document.createElement(element_tag);
 
@@ -25,53 +30,78 @@ export function createForm() {
       element_itself.setAttribute("id", item_id);
       element_itself.setAttribute("name", name);
       element_itself.setAttribute("label", label_text);
-      field_label.innerText = '\n' + label_text;
+      field_label.innerText = "\n" + label_text;
       element_itself.setAttribute("type", element_type);
-
     }
 
-    createFormElement("title","title_input","Title:","input","text");
-    createFormElement("desc","description_input","Description:","textarea");
-    createFormElement("due","due_date_input","Due Date:","input","date");
-    createFormElement("priority","priority_number_input","Priority:","input","number");
+    createFormElement("title", "title_input", "Title:", "input", "text");
+    createFormElement("desc", "description_input", "Description:", "textarea");
+    createFormElement("due", "due_date_input", "Due Date:", "input", "date");
+    createFormElement(
+      "priority",
+      "priority_number_input",
+      "Priority:",
+      "input",
+      "number",
+    );
     // optional elements for post-completion: notes, checklist
 
     //stragglers for the constructor above
     const priority = document.getElementById("priority");
-    const priority_min = 1
+    const priority_min = 1;
     const priority_max = 3;
-    priority.setAttribute("min",priority_min);
-    priority.setAttribute("max",priority_max);
-
+    priority.setAttribute("min", priority_min);
+    priority.setAttribute("max", priority_max);
   }
 
-    createFieldSet();
-};
+  createFieldSet();
+}
 
 export function displayTask() {
-  let title = document.getElementById("title");
+  function Task(title, desc, due, priority) {
+    this.title = 
+    document.getElementById("title").value;
+    this.description = document.getElementById("desc").value;
+    this.due_date = document.getElementById("due").value;
+    this.priority = document.getElementById("priority").value;
+  }
 
-  console.log("Title is: " + title.value);
+  const task = new Task();
+
+  console.log("the task is ", task)
+  console.log(JSON.stringify(task));
+
+  const task_list = [];
+  task_list.push(task);
+  console.log("the whole list is: ", task_list);
+
+  // const task1 = new Task(
+  //   document.getElementById("title").value,
+  //   document.getElementById("desc").value,
+  //   document.getElementById("due").value,
+  //   document.getElementById("priority").value,
+  // );
+
+  // let title = document.getElementById("title");
+
+  // console.log("Title is: " + title.value);
 
   function createOutputs() {
     (function createTitle() {
-      let p = document.createElement("p");
-
-      if (title.value === "") {
-        console.log("I received blank input");
-        return;
-      }
-
-      document.body.appendChild(p);
-
-      p.innerText = title.value;
+      // let p = document.createElement("p");
+      // if (title.value === "") {
+      //   console.log("I received blank input");
+      //   return;
+      // }
+      // document.body.appendChild(p);
+      // p.innerText = title.value;
     })();
   }
 
   //make this apply to all!
   function clearInputFields() {
     title.value = "";
-    
+    desc.value = "";
   }
 
   createOutputs();
@@ -80,4 +110,4 @@ export function displayTask() {
   // const clearField = () => {
   //     title.value = "";
   //   }
-};
+}
