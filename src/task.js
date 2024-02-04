c1c23ec667ff76941fc6e52e287b2a9295d7a788
Kris <109919530+maleker86@users.noreply.github.com>
2024-02-03 20:40:10 -0800

    add dynamic styling for titles. not push empty title tasks

--- src/task.js
+++ src/task.js
@@ -56,12 +56,25 @@ export function createForm() {
 
   function createSubmitButton() {
     let button = document.createElement("button");
-    button.setAttribute("type","button");
+    button.setAttribute("type", "button");
 
     form.appendChild(button);
 
     button.innerText = "Submit the info!";
     button.addEventListener("click", CreateTask);
+    button.addEventListener("click", clearInputFields)
+  }
+
+  function clearInputFields() {
+    let title = document.getElementById("title");
+    let desc = document.getElementById("desc");
+    let priority = document.getElementById("priority");
+    let due = document.getElementById("due");
+  
+    title.value = "";
+    desc.value = "";
+    priority.value = "";
+    due.value = "";
   }
 
   createFieldSet();
@@ -83,19 +96,22 @@ function CreateTask() {
   // console.log("Title is: " + title.value);
   // console.log(task);
 
-  task_list.push(task);
+  console.log("the array is:", task);
+  console.log(`the title is ${task.title}`);
 
-  function clearInputFields() {
-    let title = document.getElementById("title");
-    let desc = document.getElementById("desc");
-    let priority = document.getElementById("priority");
-    let due = document.getElementById("due");
-
-    title.value = "";
-    desc.value = "";
-    priority.value = "";
-    due.value = "";
+  if (task.title == "") {
+    logError();
+    return;
   }
+
+  function logError() {
+    console.log("Please add a title to continue!");
+  }
+
+    task_list.unshift(task);
+
+    console.log("this task list is ", task_list); 
+
 
   function displayTask() {
     // console.log("the due date is", task_list[0].due_date);
@@ -107,32 +123,53 @@ function CreateTask() {
       let task_box = document.createElement("div");
 
       console.log("We found ", i + 1, " object(s)");
+      console.log("the item is ", task_list[i]);
 
       //clear previous inputs so they do not duplicate!
 
+      const values = Object.values(task_list[i]);
+      const keys = Object.keys(task_list[i]);
+
+      // console.log("values are", values);
+      // console.log("the 1st value itself is:", values[0]);
+      // console.log("keys are", keys);
+      // console.log("the 1st key itself is:", keys[0]); 
+
       container.append(task_box);
       task_box.classList.add("task");
+
       // let p = document.createElement("p");
       //       container.append(p);
       //       p.innerText = i;
       // const keys = Object.keys(task_list[i]);
-      const values = Object.values(task_list[i]);
 
       // console.log(keys);
       // console.log(values);
-      values.forEach((value) => {
-        if (value == "") {
-          console.log("This value contains nothing");
+
+      // values.forEach((value) => {
+      //   console.log(value);
+      //   let p = document.createElement("p");
+      //   task_box.append(p);
+      //   p.innerText = value;
+      // });
+
+      for (let i = 0; i < values.length; i++) {
+        // console.log("value is", values[i]);
+        let p = document.createElement("p");
+        let h2 = document.createElement("h2");
+        if (keys[i] == "title") {
+          task_box.append(h2);
+          h2.setAttribute("class",keys[i]);
+          h2.innerText = values[i];
         } else {
-          console.log(value);
-          let p = document.createElement("p");
-          task_box.append(p);
-          p.innerText = value;
+        task_box.append(p);
+        // console.log("This is from the item called:", keys[i]);
+        p.setAttribute("class",keys[i]);
+        p.innerText = values[i];
         }
-      });
+      }
     }
   }
 
-  clearInputFields();
   displayTask();
 }
